e0a3458b42ff48ac2a5fd06fd9e6bcb80a2f0614
AnnaVihrogonova <anna.vihrogonova@acuris.com>
2019-12-26 23:17:50 +0000

    install and configure bootstrap

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,11 +1,12 @@
+import 'bootstrap'
 import './hello-world-button.scss'
-
 class HelloworldButton {
   buttonCssClass = 'hello-world-button'
   render(){
     const button = document.createElement('button')
     button.innerHTML = 'Hello world'
     button.classList.add(this.buttonCssClass)
+    button.classList.add('p-4')
     const body = document.querySelector('body')
     button.onclick = function(){
       const p = document.createElement('p')
--- src/imagePage.js
+++ src/imagePage.js
@@ -1,6 +1,8 @@
 import Heading from './components/heading/heading'
 import Image from './components/image/image'
 import _ from 'lodash'
+import 'bootstrap'
+import './index.scss'
 const image = new Image()
 image.render()
 const heading = new Heading()
