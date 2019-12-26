f46e62404b21401aa4e4c0844e3a2ecef64bd26d
AnnaVihrogonova <anna.vihrogonova@acuris.com>
2019-12-26 22:46:55 +0000

    handle server side rendering

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
 
-app.get('/', function(req, res) {
-  res.send('Some content')
+app.get('/helloPage/', function(req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/helloPage.html')
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8')
+  res.send(contentFromHtmlFile)
 })
+
+app.get('/imagePage/', function(req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/imagePage.html')
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8')
+  res.send(contentFromHtmlFile)
+})
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')))
 
 app.listen(3000, function() {
   console.log('Application is running on http://localhost:3000')
